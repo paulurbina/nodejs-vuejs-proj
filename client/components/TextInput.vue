53fd0fd8ca11586bb74d298930c3462f42f13435
paulurbina <paulurbina94@gmail.com>
2020-04-18 01:00:04 -0500

    componente person input register

---
+++ client/components/TextInput.vue
@@ -0,0 +1,31 @@
+<template>
+    <input
+        :type="type"
+        :value="value"
+        @input="$emit('input', $event.target.value)"
+        class="w-full text-xs bg-gray-200 py-3 mb-3 focus:outline text-brown-300 p-3"
+        :placeholder="placeholder"
+    />
+</template>
+
+<script>
+export default {
+    props: {
+        placeholder: {
+            type: String,
+            required: true,
+        },
+        type: {
+            type: String,
+            required: false,
+            default: 'text'
+        }
+        ,
+        value: {
+            type: String,
+            required: false,
+            default: ''
+        }
+    }
+}
+</script>
--- client/pages/Register.vue
+++ client/pages/Register.vue
@@ -1,24 +1,29 @@
 <template>
     <div class="container my-16 w-full h-12 mx-auto">
         <div class="max-w-xs mx-auto h-12">
-
             <h1 class="text-center text-lg text-orange-600">Register</h1>
             <div class="w-full rounded-sm bg-white shadow mt-5 p-8">
+                <text-input v-model="model.name" placeholder="Enter your name" />
+                <text-input v-model="model.email" placeholder="Enter your email" />
+                <text-input type="password" v-model="model.password" placeholder="Enter your password" />
 
-                    <input type="text" 
-                            class="w-full text-xs bg-gray-200 py-3 mb-3 focus:outline text-brown-300 p-3"
-                            placeholder="enter your name">
-                     <input type="text" 
-                            class="w-full text-xs bg-gray-200 py-3 mb-3 focus:outline text-brown-300 p-3"
-                            placeholder="enter your email">
-                     <input type="text" 
-                            class="w-full text-xs bg-gray-200 py-3 focus:outline text-brown-300 p-3"
-                            placeholder="enter your password">
-                    <button class="w-full text-sm mt-3 py-4 bg-green-400 text-white rounded-sm focus:outline-none hover:bg-green-600">
-                        Sign Up
-                    </button>
-
+                <button
+                    class="w-full text-sm mt-3 py-4 bg-green-400 text-white rounded-sm focus:outline-none hover:bg-green-600"
+                >Sign Up</button>
             </div>
         </div>
     </div>
 </template>
+
+<script>
+
+export default {
+    data: () => ({
+        model: {
+            name: '',
+            email: '',
+            password: '',
+        }
+    })
+}
+</script>
